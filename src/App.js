fee74e4f1b17ba65ffeb0d814ba36c077f6c16f9
Joey Imlay <jo.imlay@gmail.com>
2019-10-03 15:11:31 +0100

    Changed state, updated manifest

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
-
 import Greeter from './Greeter';
 import EmojiSearch from './EmojiSearch';
 import DictionarySearch from './DictionarySearch';
 import Calculator from './Calculator';
 import Kanban from './Kanban';
-
 import Chooser from './Chooser';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      visible: null,
+      isEmojiActive: false,
+      isDictionaryActive: false,
+      isCalculatorActive: false,
+      isKanbanActive: false
     };
+    this.openEmoji = this.openEmoji.bind(this);
+    this.openDictionary = this.openDictionary.bind(this);
+    this.openCalculator = this.openCalculator.bind(this);
+    this.openKanban = this.openKanban.bind(this)
   }
 
-  handleClick = buttonName => {
-    this.setState({ visible: buttonName });
-  };
+  openEmoji = () => {
+    this.setState({ 
+      emojiActive: true,
+      dictionaryActive: false,
+      calculatorActive: false,
+      kanbanActive: false
+    })
+  }
+
+  openDictionary = () => {
+    this.setState({
+      emojiActive: false,
+      dictionaryActive: true,
+      calculatorActive: false,
+      kanbanActive: false
+    })
+  }
+
+  openCalculator = () => {
+    this.setState({
+      emojiActive: false,
+      dictionaryActive: false,
+      calculatorActive: true,
+      kanbanActive: false})
+  }
+
+  openKanban = () => {
+    this.setState({
+      emojiActive: false,
+      dictionaryActive: false,
+      calculatorActive: false,
+      kanbanActive: true
+    })
+  }
 
   render() {
     return (
@@ -30,22 +65,22 @@ class App extends React.Component {
           </section>
 
           <section className="right">
-            {this.state.visible === "emoji" && (
+            {this.state.emojiActive && (
               <div className="emoji">
                 <EmojiSearch />
               </div>
             )}
-            {this.state.visible === "dictionary" && (
+            {this.state.dictionaryActive && (
               <div className="dictionary">
                 <DictionarySearch />
               </div>
             )}
-            {this.state.visible === "calculator" && (
+            {this.state.calculatorActive && (
               <div className="calculator">
                 <Calculator />
               </div>
             )}
-            {this.state.visible === "kanban" && (
+            {this.state.kanbanActive && (
               <div className="kanban">
                 <Kanban />
               </div>
@@ -55,10 +90,10 @@ class App extends React.Component {
         </section>
 
         <footer>
-          <Chooser handleClick={this.handleClick} appName="emoji" />
-          <Chooser handleClick={this.handleClick} appName="dictionary" />
-          <Chooser handleClick={this.handleClick} appName="calculator" />
-          <Chooser handleClick={this.handleClick} appName="kanban" />
+          <Chooser handleClick={this.openEmoji} appName="emoji" />
+          <Chooser handleClick={this.openDictionary} appName="dictionary" />
+          <Chooser handleClick={this.openCalculator} appName="calculator" />
+          <Chooser handleClick={this.openKanban} appName="kanban" />
           <span>pinpin</span>
         </footer>
         
